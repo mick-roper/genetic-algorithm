9db0418088cd498cfc809c1a820515ec9f535c4a
mick-roper <mick.roper@dazn.com>
2019-08-13 09:06:27 +0100

    added an entry arg

--- src/main.go
+++ src/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"time"
 
 	"github.com/mick-roper/genetic-algorithm/src/algorithm"
 )
 
-const target = "Hello World"
+var target = flag.String("target", "", "the target string we trying to reach")
 
 func main() {
-	pop := algorithm.NewPopulation(100, len(target))
+	flag.Parse()
 
-	for pop.Fittest().Chromosome() != target {
-		pop.Iterate(target)
+	if *target == "" {
+		log.Fatal("a target must be provided")
+	}
+
+	pop := algorithm.NewPopulation(100, len(*target))
+
+	for pop.Fittest().Chromosome() != *target {
+		pop.Iterate(*target)
 		pop.Print()
 		time.Sleep(100 * time.Millisecond)
 	}
