29023c28c95bc04551f3be5b4b9b00e4b47c7ebb
mick-roper <mick.roper@dazn.com>
2019-08-13 09:45:40 +0100

    made stuff more tricky

--- src/main.go
+++ src/main.go
@@ -12,6 +12,8 @@ var target = flag.String("target", "", "the target string we trying to reach")
 var delayMs = flag.Int("delay", 25, "the amount of delay between iterations in milliseconds")
 var popSize = flag.Int("population", 100, "the size of the population you want to use")
 
+var start, diff int64
+
 func main() {
 	flag.Parse()
 
@@ -19,13 +21,17 @@ func main() {
 		log.Fatal("a target must be provided")
 	}
 
-	interval := time.Duration(*delayMs) * time.Millisecond
+	interval := int64(*delayMs)
 	pop := algorithm.NewPopulation(*popSize, len(*target))
 
 	for pop.Fittest().Chromosome() != *target {
+		start = time.Now().UnixNano()
 		pop.Iterate(*target)
 		pop.Print()
-		<-time.After(interval)
+
+		diff = (time.Now().UnixNano() - start) / 100000
+
+		<-time.After(time.Duration(interval-diff) * time.Nanosecond)
 	}
 
 	log.Println("Population completed its evolution at generation ", pop.Generation)
